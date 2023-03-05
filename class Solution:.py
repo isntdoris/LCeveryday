75866320845f9f60f41be5136a59c1051c23fd2e
isntdoris <isntdoris@gmail.com>
2023-03-05 17:10:31 -0600

    annotation

--- class Solution:.py
+++ class Solution:.py
@@ -1,25 +1,26 @@
 class Solution:
-    def addBinary(self, a, b):
+    def addBinary(self, a: str, b: str) -> str:
+        
         result = []
 
         carry = 0
 
         i, j = len(a)-1, len(b)-1
 
-        while i >= 0 or j >= 1 or carry:
-            total = carry # this concept is critical
+        while i >= 0 or j >= 0 or carry:
+            
+            total = carry
 
             if i >= 0:
                 total += int(a[i])
                 i -= 1
-
+            
             if j >= 0:
                 total += int(b[j])
                 j -= 1
 
             result.append(str(total %2)) # using the remainder technique
-            
+
             carry = total // 2 # dividing the sum by 2 to get the carry-over value
 
         return ''.join(reversed(result))
-        
