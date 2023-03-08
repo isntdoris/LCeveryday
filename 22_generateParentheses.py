c80f0b5e4dbaea978d4b33e8ce725f42183839fc
isntdoris <isntdoris@gmail.com>
2023-03-08 11:42:20 -0600

    update #22 explanation

--- 22_generateParentheses.py
+++ 22_generateParentheses.py
@@ -3,6 +3,8 @@
 # 3 .If left < n then we can add '(' to the current string
 # 4. If right < left then we can add ')' to the current string
 
+# **need to think about how to make sure to add ')' only after valid '('**
+
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         def dfs(left, right, s):
@@ -10,12 +12,12 @@ class Solution:
                 res.append(s)
                 return 
 
-            if left < n:
+            if left < n: # Does this mean that it will always start from (1, 0, '(')? Ans: Yes, I think so..
                 dfs(left + 1, right, s + '(')
 
             if right < left:
                 dfs(left, right + 1, s + ')')
 
-        res = []
+        res = [] # initiate in the beginning reduces the efficiency somehow
         dfs(0, 0, '')
         return res
