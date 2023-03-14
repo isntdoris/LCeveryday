d8e19856d174d47c8c9bc345a00ffe6e0c45d403
isntdoris <isntdoris@gmail.com>
2023-03-14 10:37:24 -0500

    update #382

--- 382_linkedListRandomNode.py
+++ 382_linkedListRandomNode.py
@@ -1,1 +1,28 @@
 # I have difficulty understanding this question..
+# Hint: convert a singly linked list to an array
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+
+    def __init__(self, head: ListNode):
+        """
+        @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node.
+        """
+
+        # convert singly linked list to an array
+        self.range = []
+        while head: 
+            self.range.append(head.val)
+            head = head.next
+            
+    def getRandom(self) -> int:
+        """
+        Returns a random node's value.
+        """
+        pick = int(random.random() * len(self.range))
+        return self.range[pick]
