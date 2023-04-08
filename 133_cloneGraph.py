1f27577e195adc4f7ccd00690cd1db0382268eb5
isntdoris <isntdoris@gmail.com>
2023-04-08 08:24:22 -0500

    update #133

---
+++ 133_cloneGraph.py
@@ -0,0 +1,19 @@
+# 1. BFS to traverse the graph
+# 2. A hash map to keep track of already visited and already cloned nodes
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if not node: return node
+        
+        q, clones = deque([node]), {node.val: Node(node.val, [])}
+        while q:
+            cur = q.popleft() 
+            cur_clone = clones[cur.val]            
+
+            for ngbr in cur.neighbors:
+                if ngbr.val not in clones:
+                    clones[ngbr.val] = Node(ngbr.val, [])
+                    q.append(ngbr)
+                    
+                cur_clone.neighbors.append(clones[ngbr.val])
+                
+        return clones[node.val]
