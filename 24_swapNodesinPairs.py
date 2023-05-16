fa0bb6569c25ffb7179e9fa4245dc52ffe068fa7
isntdoris <isntdoris@gmail.com>
2023-05-16 15:37:57 -0500

    update for LC 24

--- 24_swapNodesinPairs.py
+++ 24_swapNodesinPairs.py
@@ -5,23 +5,13 @@
 #         self.next = next
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # if not head: return head
-
-        # prev, cur, ans = None, head, head.next
-        # while cur and cur.next:
-        #     adj = cur.next
-        #     if prev: prev.next = adj
-
-        #     cur.next, adj.next = adj.next, cur
-        #     prev, cur = cur, cur.next
-
-        # return ans or head
-
         if not head: return head
         prev, cur, ans = None, head, head.next
+
         while cur and cur.next:
             adj = cur.next
             if prev: prev.next = adj
             cur.next, adj.next = adj.next, cur
             prev, cur = cur, cur.next
+        
         return ans or head
