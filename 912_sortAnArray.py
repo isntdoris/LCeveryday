51e3697d31ee9d0058b37fb2a04b5b4ea5f2021f
isntdoris <isntdoris@gmail.com>
2023-06-17 15:16:30 -0500

    update for LC 912

--- 912_sortAnArray.py
+++ 912_sortAnArray.py
@@ -3,7 +3,6 @@ class Solution:
         def merge(arr, L, M, R):
             left, right = arr[L:M+1], arr[M+1:R+1]
             i, j, k = L, 0, 0
-
             while j < len(left) and k < len(right):
                 if left[j] <= right[k]:
                     arr[i] = left[j]
@@ -24,10 +23,10 @@ class Solution:
         def mergeSort(arr, l, r):
             if l == r:
                 return arr
-
-            m = (l+r)//2
+            m = (l + r) // 2
             mergeSort(arr, l, m)
-            mergeSort(arr, m+1, r)
+            mergeSort(arr, m + 1, r)
             merge(arr, l, m, r)
-
-        return arr
+            return arr
+        
+        return mergeSort(nums, 0, len(nums) - 1)
