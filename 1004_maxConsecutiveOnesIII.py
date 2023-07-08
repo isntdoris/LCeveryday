fb8bb24e78c63a5368fc4a1b6600b265d4dd913c
isntdoris <isntdoris@gmail.com>
2023-07-08 07:57:38 -0500

    update LC 1004

--- 1004_maxConsecutiveOnesIII.py
+++ 1004_maxConsecutiveOnesIII.py
@@ -1,19 +1,20 @@
 class Solution:
-    def longestOnes(self, A: List[int], K: int) -> int:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+
       left = right = 0
       
-      for right in range(len(A)):
-        # if we encounter a 0 the we decrement K
-        if A[right] == 0:
-          K -= 1
-        # else no impact to K
+      for right in range(len(nums)):
+        # if we encounter a 0 the we decrement k
+        if nums[right] == 0:
+          k -= 1
+        # else no impact to k
         
-        # if K < 0 then we need to move the left part of the window forward
+        # if k < 0 then we need to move the left part of the window forward
         # to try and remove the extra 0's
-        if K < 0:
-          # if the left one was zero then we adjust K
-          if A[left] == 0:
-            K += 1
+        if k < 0:
+          # if the left one was zero then we adjust k
+          if nums[left] == 0:
+            k += 1
           # regardless of whether we had a 1 or a 0 we can move left side by 1
           # if we keep seeing 1's the window still keeps moving as-is
           left += 1
