f87035459a6b5276bdee549998e39bd08b00f8d3
isntdoris <isntdoris@gmail.com>
2023-08-01 21:46:47 -0500

    update LC 697

--- 697_degreeofanArray.py
+++ 697_degreeofanArray.py
@@ -6,11 +6,12 @@ class Solution:
             d[value].append(idx)
 
         # 2. get the max degree
-        m = max(len(v) for v in d.values())
+        m = max([len(v) for v in d.values()])
 
         # 3. find shortest span for max degree
         best = len(nums)
 
         for v in d.values():
-            best = min(best, v[-1] - v[0] + 1)
+            if len(v) == m:
+                best = min(best, v[-1] - v[0] + 1)
         return best
