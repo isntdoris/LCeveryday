9cf5fae781f126b22e15fd963252bc53f97b8dd9
isntdoris <isntdoris@gmail.com>
2023-08-12 09:24:14 -0500

    update LC 933

---
+++ 933_numberofRecentCalls.py
@@ -0,0 +1,11 @@
+class RecentCounter:
+
+    def __init__(self):
+        self.queue = deque()
+
+    def ping(self, t: int) -> int:
+        self.queue.append(t)
+
+        while self.queue[o] < t - 3000:
+            self.queue.popleft()
+        return len(self.queue)
