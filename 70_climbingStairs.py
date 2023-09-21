14908c8a5a51eaaee935854f05853dcc7306401a
isntdoris <isntdoris@gmail.com>
2023-09-21 12:48:12 -0500

    update LC 70

--- 70_climbingStairs.py
+++ 70_climbingStairs.py
@@ -18,11 +18,11 @@ class Solution:
         if n == 1: return 1
         if n == 2: return 2
 
-        dp = [0]*(n+1) # considering zero steps we need n+1 places
+        dp = [0] * (n + 1) # considering zero steps we need n+1 places
         dp[1] = 1
         dp[2] = 2
         for i in range(3, n + 1): # n+1 (3, n+1)
-            dp[i] = dp[i-1] + dp[i-2]
+            dp[i] = dp[i - 1] + dp[i - 2]
         print(dp)
         return dp[n]
 
