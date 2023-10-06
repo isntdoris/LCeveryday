6095b5a9ffd2f1d9ea843d90e1c7cadcfba03abd
isntdoris <isntdoris@gmail.com>
2023-10-06 11:51:57 -0500

    update LC 33

---
+++ 33_searchinRotatedSortedArray.py
@@ -0,0 +1,24 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # [4,5,6,7,0,1,2]
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            m = (l + r) // 2
+
+            if nums[m] == target:
+                return m
+            
+            # left portion
+            if nums[l] <= nums[m]: # here "<="
+                if target > nums[m] or target < nums[l]:
+                    l = m + 1
+                else:
+                    r = m - 1
+            # right portion
+            else:
+                if target < nums[m] or target > nums[r]:
+                    r = m - 1
+                else:
+                    l = m + 1
+        return -1             
