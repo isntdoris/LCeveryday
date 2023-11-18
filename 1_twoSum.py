bad384a363264951e6423a39c0ca6f1963b01bb4
isntdoris <isntdoris@gmail.com>
2023-11-18 15:35:22 -0600

    updated LC 1

--- 1_twoSum.py
+++ 1_twoSum.py
@@ -1,6 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashMap = {}
+        hashMap = {} # {num: index}
 
         for index, num in enumerate(nums):
             diff = target - num
