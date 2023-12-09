160c590fc88f2a6fca8deb4d97d87db136cf9ecb
isntdoris <isntdoris@gmail.com>
2023-12-08 18:00:31 -0600

    updated LC 180

--- 180_consecutiveNumbers.sql
+++ 180_consecutiveNumbers.sql
@@ -9,3 +9,16 @@ WITH cte AS (
 SELECT DISTINCT num ConsecutiveNums
 FROM cte
 WHERE (num=num1) AND (num=num2)
+
+-- Editorial Solution
+SELECT DISTINCT l1.Num AS ConsecutiveNums
+FROM
+    Logs l1,
+    Logs l2,
+    Logs l3
+WHERE
+    l1.Id = l2.Id - 1
+    AND l2.Id = l3.Id - 1
+    AND l1.Num = l2.Num
+    AND l2.Num = l3.Num
+;
