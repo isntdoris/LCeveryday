7881c123f93085d32e8d627d91e2846dab066f42
isntdoris <isntdoris@gmail.com>
2023-12-10 15:08:51 -0600

    updated LC 1164

---
+++ 1164_Product_Price_at_a_Given_Date.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+WITH CTE AS
+(SELECT *, RANK() OVER(PARTITION BY product_id ORDER BY change_date DESC) AS r
+FROM Products
+WHERE change_date <= '2019-08-16')
+
+SELECT product_id, new_price AS price
+FROM CTE
+WHERE r = 1
+
+UNION
+
+SELECT product_id, 10 AS price
+FROM Products
+WHERE product_id NOT IN (SELECT product_id FROM CTE)
