505f86a0b08da0c7907d89b195ae1fd7d9908ea6
isntdoris <isntdoris@gmail.com>
2023-12-10 17:36:34 -0600

    updated LC 585

---
+++ 585_Investments_in_2016.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016
+FROM
+(SELECT *, COUNT(*) OVER(PARTITION BY tiv_2015) AS tiv_2015_cnt, COUNT(*) OVER(PARTITION BY lat, lon) AS loc_cnt
+FROM Insurance) tmp
+WHERE tiv_2015_cnt > 1 AND loc_cnt = 1
