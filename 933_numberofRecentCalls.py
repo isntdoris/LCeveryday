f7f841e3af50a336c1120cc06737b2135124fb14
isntdoris <isntdoris@gmail.com>
2024-03-09 10:08:24 -0600

    933

--- 933_numberofRecentCalls.py
+++ 933_numberofRecentCalls.py
@@ -6,6 +6,6 @@ class RecentCounter:
     def ping(self, t: int) -> int:
         self.queue.append(t)
 
-        while self.queue[o] < t - 3000:
+        while self.queue[0] < t - 3000:
             self.queue.popleft()
         return len(self.queue)
