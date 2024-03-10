2776102e142d33f446b99c3ae1b1b258ae486cdc
isntdoris <isntdoris@gmail.com>
2024-03-09 18:36:06 -0600

    394

--- 394_Decode_String.py
+++ 394_Decode_String.py
@@ -1,7 +1,7 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        # time: O(n * k)?
-        # space: O(n)?
+        # time: O(maxK^countK⋅n) / O(n * k)?
+        # space: O(sum(maxK^countK⋅n)) / O(n)?
         # s = "3[a2[c]d2[b]]"
         stack = []
 
