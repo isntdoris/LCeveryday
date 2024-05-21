2d888ea6ab3cac2d93edcdc5483f405b3fcdca37
isntdoris <isntdoris@gmail.com>
2024-05-21 09:51:13 -0500

    78

--- 78_subsets.py
+++ 78_subsets.py
@@ -16,3 +16,17 @@ class Solution:
         
         dfs(0)
         return res
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        def backtrack(start, path):
+            result.append(path[:]) # RICKY HERE; or path.copy()
+
+            for i in range(start, len(nums)):
+                path.append(nums[i])
+                backtrack(i + 1, path)
+                path.pop()
+
+        backtrack(0, [])
+        return result
