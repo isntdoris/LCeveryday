0df90a50c65b9c82afc51756d2ee9cbc49854f1e
isntdoris <isntdoris@gmail.com>
2024-07-06 08:25:27 -0500

    update 74

--- 74_searcha2DMatrix.py
+++ 74_searcha2DMatrix.py
@@ -1,5 +1,34 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # sol 2:
+                # if not matrix or not matrix[0]:
+        #     return False
+        
+        high = 0
+        low = len(matrix) - 1
+        left = 0
+        right = len(matrix[0]) - 1
+
+        while high <= low:
+            mid_row = (high + low) // 2
+
+            if matrix[mid_row][0] > target:
+                low = mid_row - 1
+            elif matrix[mid_row][-1] < target:
+                high = mid_row + 1
+            else:
+
+                while left <= right:
+                    mid_col = (left + right) // 2
+                    if matrix[mid_row][mid_col] > target:
+                        right = mid_col - 1
+                    elif matrix[mid_row][mid_col] < target:
+                        left = mid_col + 1
+                    else:
+                        return True
+                return False
+        
+        # sol 1:
         # https://www.youtube.com/watch?v=Ber2pi2C0j0
         ROWS, COLS = len(matrix), len(matrix[0])
 
