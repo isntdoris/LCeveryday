a49e88917d96b398629bb772d1a0e03e34f1da4a
isntdoris <isntdoris@gmail.com>
2024-09-17 07:35:24 -0500

    leetcode #884

---
+++ 884_Uncommon_Words_from_Two_Sentences.py
@@ -0,0 +1,13 @@
+from collections import Counter
+class Solution:
+    def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
+        count = {}
+
+        count = Counter(s1.split())
+        count += Counter(s2.split())
+        # for word in s1.split():
+        #     count[word] = count.get(word, 0) + 1
+        # for word in s2.split():
+        #     count[word] = count.get(word, 0) + 1
+
+        return [word for word in count if count[word] == 1]
